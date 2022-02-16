c7e3943af29a269ed3f2f33cf5f76899379be829
kongeee <furkanekici1997@gmail.com>
2022-02-16 15:36:26 +0300

    springboot

---
+++ Kamp/springboot/northwind/src/main/java/com/turkcell/northwind/business/dtos/ProductListDto.java
@@ -0,0 +1,18 @@
+package com.turkcell.northwind.business.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductListDto {
+	
+	private int productId;
+	private String productName;
+	private double unitPrice;
+	private String quantityPerUnit;
+	private int unitsInStock;
+
+}
